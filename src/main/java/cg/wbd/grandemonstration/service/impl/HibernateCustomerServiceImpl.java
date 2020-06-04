e44610d9b12227fd966f75ab15667d7cfd19921b
linhhm2406 <linh.hm24@gmail.com>
2020-06-04 09:17:40 +0700

    submit bai tap

---
+++ src/main/java/cg/wbd/grandemonstration/service/impl/HibernateCustomerServiceImpl.java
@@ -0,0 +1,75 @@
+package cg.wbd.grandemonstration.service.impl;
+
+import cg.wbd.grandemonstration.model.Customer;
+import cg.wbd.grandemonstration.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.Collections;
+import java.util.List;
+
+@Transactional
+public class HibernateCustomerServiceImpl implements CustomerService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Customer> findAll() {
+        TypedQuery<Customer> query = entityManager.createQuery("select c from Customer as c",Customer.class);
+        return query.getResultList();
+
+    }
+
+    @Override
+    public Customer findOne(Long id) {
+        String queryStr = "SELECT c FROM Customer AS c WHERE c.id = :id";
+        TypedQuery<Customer> query = entityManager.createQuery(queryStr, Customer.class);
+        query.setParameter("id", id);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> save(List<Customer> customers) {
+        return null;
+    }
+
+    @Override
+    public boolean exists(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<Customer> findAll(List<Long> ids) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(Long id) {
+    }
+
+    @Override
+    public void delete(Customer customer) {
+    }
+
+    @Override
+    public void delete(List<Customer> customers) {
+    }
+
+    @Override
+    public void deleteAll() {
+    }
+}
